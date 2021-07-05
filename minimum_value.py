14278522e91036889c68f0c0e09375cf172d6e1d
Ismael Jarmouni Gómez <ijarmounigomez@gmail.com>
2021-07-05 19:19:36 +0200

    Update minimum_value.py

--- minimum_value.py
+++ minimum_value.py
@@ -1,1 +1,7 @@
-def minimum_value
+def minimum_value(list):
+  smallest_until_now = list[0] # We set the smallest number found until now to be the first value of the list.
+  for number in list: # Now we iterate through each value in the list.
+    if number < smallest_until_now: # With this "if" statement, if a smaller value than the one set at the beginning is found, that value will be the new smallest_until_now. 
+      smallest_until_now = number
+  return smallest_until_now
+    
